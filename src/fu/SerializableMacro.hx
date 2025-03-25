a1c750fccb3aa1715a8555b01440fc11cdc4645d
artem pecheny <pecheny@gmail.com>
2025-03-25 17:29:17 +0300

    Extract/generalize expression declaring by serializing type.

--- src/fu/SerializableMacro.hx
+++ src/fu/SerializableMacro.hx
@@ -9,6 +9,19 @@ using haxe.macro.ComplexTypeTools;
 
 class SerializableMacro {
     public static function build() {
+        var valueExprs = {
+            dump: name -> macro Reflect.setField(data, $v{name}, $i{name}),
+            load: name -> macro $i{name} = Reflect.field(data, $v{name})
+        };
+        var classExprs = {
+            dump: name -> macro Reflect.setField(data, $v{name}, $i{name}.dump()),
+            load: name -> macro $i{name}.load(Reflect.field(data, $v{name})),
+        };
+        var enumExprs = {
+            dump: name -> macro Reflect.setField(data, $v{name}, haxe.Json.parse(tink.Json.stringify($i{name}))),
+            load: name -> macro $i{name} = tink.Json.parse(haxe.Json.stringify(data.$name)),
+        };
+
         var fields = Context.getBuildFields();
         var loadExprs = new MethodExprs(fields, "load");
         loadExprs.addArg("data", macro :Dynamic);
@@ -28,20 +41,21 @@ class SerializableMacro {
                         case null:
                             Context.error('Define explicit type for $name variable to be serialized', pos);
                         case macro :Int, macro :String, macro :Float, macro :Bool:
-                            dumpExprs.push(macro Reflect.setField(data, $v{name}, $i{name}));
-                            loadExprs.push(macro $i{name} = Reflect.field(data, $v{name}));
+                            dumpExprs.push(valueExprs.dump(name));
+                            loadExprs.push(valueExprs.load(name));
+                        case TPath({name: 'Array'}):
                         case TPath(p):
                             var t:Type = ct.toType();
                             switch t {
                                 case TInst(_.get() => ct, params):
                                     if (ct.interfaces.filter(f -> f.t.get().name == "Serializable").length < 1)
-                                        throw '$ct doesnt implement Serializable';
-                                    dumpExprs.push(macro Reflect.setField(data, $v{name}, $i{name}.dump()));
-                                    loadExprs.push(macro $i{name}.load(Reflect.field(data, $v{name})));
+                                        Context.error('${ct.name} doesnt implement Serializable', pos);
+                                    dumpExprs.push(classExprs.dump(name));
+                                    loadExprs.push(classExprs.load(name));
 
                                 case TEnum(t, params):
-                                    dumpExprs.push(macro Reflect.setField(data, $v{name}, haxe.Json.parse(tink.Json.stringify($i{name}))));
-                                    loadExprs.push(macro $i{name} = tink.Json.parse(haxe.Json.stringify(data.$name)));
+                                    dumpExprs.push(enumExprs.dump(name));
+                                    loadExprs.push(enumExprs.load(name));
                                 case _:
                                     Context.error('Serialization of $t not supported', pos);
                             }
